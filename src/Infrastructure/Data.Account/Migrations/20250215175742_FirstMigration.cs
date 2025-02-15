ae35bcaad90b4d93c12758fef1fa135be86eba74
José Carlos <j-rock-fods@hotmail.com>
2025-02-15 15:06:03 -0300

    Criação da migração do sqlite e ajustes para utilizar o mesmo

---
+++ src/Infrastructure/Data.Account/Migrations/20250215175742_FirstMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Account.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "account",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(150)", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Created_At = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    Updated_At = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                    Deleted_At = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_account", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "account");
+        }
+    }
+}
